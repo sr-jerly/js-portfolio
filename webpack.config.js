d18b575d9d731cf80a20594c79fb48fb51ed11ea
sr-jerly <jerlymigueldelarosah@gmail.com>
2021-11-06 14:46:39 -0400

    add --> babel

--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,24 @@ const path = require('path');
 /** @type {import('webpack').Configuration} */
 
 
-module.exports ={
-    entry: './src/index.js',
-    output:{
-        path: path.resolve(__dirname, 'dist'),
-        filename:"main.js"
-    },
-    resolve:{
-        extensions: ['.js']
-    }
-}
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+  },
+  resolve: {
+    extensions: [".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_module/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+};
