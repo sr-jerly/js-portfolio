19d7d1f55ce1a979de1e2d4461c3e77442106556
sr-jerly <jerlymigueldelarosah@gmail.com>
2021-11-08 20:12:06 -0400

    add -->  copy-webpack-plugin'

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
 
 /** @type {import('webpack').Configuration} */
 
@@ -43,6 +45,14 @@ module.exports = {
       template: "./public/index.html",
       filename: "index.html",
     }),
-    new MiniCssExtractPlugin()
+    new MiniCssExtractPlugin(),
+    new CopyPlugin({
+      patterns:[
+        {
+          from: path.resolve(__dirname, "src", "assets/images"),
+          to:"assets/images"
+      }
+      ]
+    })
   ],
 };
